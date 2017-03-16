82844dc92992feaa0251975d8b8f3d393f797f8d
bhuvana09 <bhuvaneswari.m2@cognizant.com>
2017-03-16 18:47:07 +0530

    commit

---
+++ sampleProjectGit/src/sampleProjectGit/TestingTwo.java
@@ -0,0 +1,10 @@
+package sampleProjectGit;
+
+public class TestingTwo {
+void method1(){
+	int test=10;
+	String str="testdata";
+	System.out.println("test:"+ test);
+	System.out.println("String:"+ str);
+}
+}
