2a838bc72453c2cbb92446f484fee6431b7d309e
bhuvana09 <bhuvaneswari.m2@cognizant.com>
2017-04-04 10:24:29 +0530

    committting

--- sampleProjectGit/src/sampleProjectGit/TestingOne.java
+++ sampleProjectGit/src/sampleProjectGit/TestingOne.java
@@ -3,7 +3,6 @@ package sampleProjectGit;
 public class TestingOne {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 			System.out.println("testing a");
 			System.out.println("testing b");
 			System.out.println("sgdsadg");
