a2bc5835d9f6e88d30159bac3eebc0febd724ac4
bhuvana09 <bhuvaneswari.m2@cognizant.com>
2017-04-07 12:18:09 +0530

    committing the changes to git

--- sampleProjectGit/src/sampleProjectGit/TestingOne.java
+++ sampleProjectGit/src/sampleProjectGit/TestingOne.java
@@ -11,6 +11,15 @@ public class TestingOne {
 			System.out.println("testing a");
 			System.out.println("testing b");
 			System.out.println("sgdsadg");
+			System.out.println("testing a");
+			System.out.println("testing b");
+			System.out.println("sgdsadg");
+			System.out.println("testing a");
+			System.out.println("sgdsadg");
+			System.out.println("testing a");
+			System.out.println("testing b");
+			System.out.println("sgdsadg");
+	
 	}
 
 }
